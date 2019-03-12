25ea5b9c3bd3c15fae18497945765e5bb28063ee
shafali03 <shaf03sm@gmail.com>
2019-03-12 20:43:43 +0000

    Building Our Components

---
+++ facedetect/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Particles from 'react-particles-js';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+const particlesOption = {
+  particles: {
+    number: {
+      value: 145,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOption}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
