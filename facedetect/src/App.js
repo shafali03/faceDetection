0521420f311155497de5796a8cdea1fd1fc0cdfc
shafali03 <shaf03sm@gmail.com>
2019-03-13 20:14:37 +0000

    Image Recognition API

--- facedetect/src/App.js
+++ facedetect/src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Particles from 'react-particles-js';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+
 import './App.css';
+
+
+//API key
+const app = new Clarifai.App({
+  apiKey: 'c5afb6ba2a7741ef801d8e8532e2d14d'
+});
+
 
 const particlesOption = {
   particles: {
@@ -19,6 +29,36 @@ const particlesOption = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+
+        this.state.input)
+      .then(
+        function (response) {
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        },
+        function (err) {
+          // there was an error
+        }
+      );
+  }
+
   render() {
     return (
       <div className="App">
@@ -28,11 +68,18 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}
+        />
+
+
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
 }
 
 export default App;
+
+
+ //!image url which will get display when we click on the buttonSubmit and get updated whatever the input is. Now we can pass the imageUrl to the faceRecognition
